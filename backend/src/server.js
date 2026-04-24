0c288cee1e633de81adfa32250e74998b8a224dc
dcarsc2 <tpr.th.bvb@gmail.com>
2026-04-24 02:53:39 -0400

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://helios-ai-six.vercel.app/"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
